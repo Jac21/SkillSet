50d2331da54af5660a0f3fa5ad5c989daf568d28
Jeremy Cantu <jcantu521@gmail.com>
2017-02-07 11:30:00 -0600

    Migrated call to json to fetch() and removed jQuery dependency because why. Added some additional error handling and favicon

--- scripts/converter.js
+++ scripts/converter.js
@@ -1,76 +1,92 @@
-$(document).ready(function(){
+// base csv string object
+var csv = "source,target,value";
 
-	// base csv string object
-	var csv = "source,target,value";
+// user has uploaded json file and clicked on submit button
+var submitButton = document.getElementById('json-input-button');
+submitButton.addEventListener('click', convertJson, false);
 
-	// user has uploaded json file and clicked on submit button
-	$("#json-input-button").on('click', convertJson);
+// user has uploaded converted csv file and clicked on submit button
+var csvInputButton = document.getElementById("csv-input-button");
+csvInputButton.addEventListener('click', visualizeCsv, false);
 
-	// convert JSONResume file input to csv file, download to user's machine
-	function convertJson(e) {
-		e.preventDefault();
+// error area declaration
+var errorArea = document.getElementById('error-area');
 
-		// grab file value, create object URL to use in getJSON call
-		var jsonFileValue = $("#json-input")[0].files[0];
-		var jsonFile = window.URL.createObjectURL(jsonFileValue);
+// convert JSONResume file input to csv file, download to user's machine
+function convertJson(e) {
+	e.preventDefault();
 
-		$.getJSON(jsonFile, function(data) {
+	// grab file value, create object URL to use in getJSON call
+	var jsonInput = document.getElementById('json-input');
+	var jsonFileValue = jsonInput.files[0];
+	var jsonFile = window.URL.createObjectURL(jsonFileValue);
 
-			// iterate through json resume skill section
-			for (var i = 0; i < data.skills.length; i++) {
-				// iterate through keywords sub-section, append values to csv
-				for (var j = 0; j < data.skills[i].keywords.length; j++) {
-								csv += "\n" + data.skills[i].name + "," + 
-								data.skills[i].keywords[j] + "," + 
-								setSkillValue(data.skills[i].level);
+	// if browser supports fetch api
+	if(self.fetch) {
+		fetch(jsonFile)
+		.then(function(response) {
+			return response.json().then(function(json) {
+				// iterate through json resume skill section
+				for (var i = 0; i < json.skills.length; i++) {
+					// iterate through keywords sub-section, append values to csv
+					for (var j = 0; j < json.skills[i].keywords.length; j++) {
+						csv += "\n" + json.skills[i].name + "," + 
+						json.skills[i].keywords[j] + "," + 
+						setSkillValue(json.skills[i].level);
+					}
 				}
-			}
+				// clear error area
+				errorArea.innerHTML = '';
 
-			// download
-			downloadCsv(csv);
-	  });
-	};
+				// download
+				downloadCsv(csv);
+			});
+		})
+		.catch(function(error) {
+		  errorArea.innerHTML = '<strong>There has been a problem with your fetch operation: ' + error.message + '</strong>';
+		});
+	} else {
+		errorArea.innerHTML = '<strong> Your browser does not support the fetch API! Please try this once more in Chrome or Firefox.</strong>';
+	}
+};
 
-	// user has uploaded converted csv file and clicked on submit button
-	$("#csv-input-button").on('click', visualizeCsv);
+// create csv object URL from file, call D3
+function visualizeCsv(e) {
+	e.preventDefault();
 
-	// hide form elements, create csv object URL from file, call D3
-	function visualizeCsv(e) {
-		e.preventDefault();
+	var csvInput = document.getElementById('csv-input');
+	var csvFileValue = csvInput.files[0];
+	var csvFile = window.URL.createObjectURL(csvFileValue);
 
-		//$("#json-form").hide();
-		//$("#csv-form").hide();
+	forceLayoutVisualize(csvFile);
+};
 
-		var csvFileValue = $("#csv-input")[0].files[0];
-		var csvFile = window.URL.createObjectURL(csvFileValue);
+/* 
+	Helper Functions
+*/
 
-		forceLayoutVisualize(csvFile);
-	};
+// set skill value numeric value from json value
+function setSkillValue(level) {
+	var skillLevelValue = "0";
 
-	// set skill value numeric value from json value
-  function setSkillValue(level) {
-  	var skillLevelValue = "0";
+	if (level === "Beginner") {
+		skillLevelValue = "1.0";
+	} else if (level === "Intermediate") {
+		skillLevelValue = "2.0";
+	} else if (level === "Advanced") {
+		skillLevelValue = "2.5";
+	}
 
-		if (level === "Beginner") {
-			skillLevelValue = "1.0";
-		} else if (level === "Intermediate") {
-			skillLevelValue = "2.0";
-		} else if (level === "Advanced") {
-			skillLevelValue = "2.5";
-		}
+	return skillLevelValue;
+};
 
-		return skillLevelValue;
-  }
+// downloads csv-converted file to local machine
+function downloadCsv(csvString) {
+	var a = document.createElement('a');
+	a.href = 'data:attachment/csv,' +  encodeURIComponent(csvString);
+	a.target = '_blank';
+	a.download = 'mySkills.csv';
 
-  // downloads csv-converted file to local machine
-  function downloadCsv(csvString) {
-  	var a = document.createElement('a');
-		a.href = 'data:attachment/csv,' +  encodeURIComponent(csvString);
-		a.target = '_blank';
-		a.download = 'mySkills.csv';
-
-		document.body.appendChild(a);
-		a.click();
-  }
-
-});
+	document.body.appendChild(a);
+	a.click();
+};
