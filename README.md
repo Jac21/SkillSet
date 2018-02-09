fcd666020b8265b987b3e970085f177eebc021c9
Jeremy Cantu <jcantu521@gmail.com>
2018-02-09 14:44:08 -0600

    Minor updates

--- README.md
+++ README.md
@@ -9,23 +9,13 @@ Live site:
 Live demo: 
 >- https://jac21.github.io/viz.html
 
-TODO:
-------
->- ~~Flesh out D3 force layout graph structure~~
->- ~~Mock-up skill graph from current skill set~~
->- ~~Set graph from JSONResume data dynamically AND/OR user input~~
->- Flesh out projects-based viz, either appending to current force layout, or creating an entirely new one
->- Tweak visual cues (~~link opacity for weaker skills~~, mouseover events to show more details)
->- Allow link and picture additions to nodes
-
 To use on your site [(like so)](https://jac21.github.io/viz.html):
 ----------------------------
->- Git clone using this repository's clone URL: https://github.com/Jac21/SkillSet.git 
->- The "dist" folder contains all you need to run and visualize: "index.html" where the render is displayed, "app.js" where the viz code is written, and "style.css" containing the necessary CSS for the visualization.
+>- Clone using this repository's clone URL (or just download it straight): https://github.com/Jac21/SkillSet.git 
+>- The "dist" folder contains all you need to run and visualize: "index.html" is where the render is displayed, "app.js" is viz code bundle, and "style.css" containing the necessary CSS for the visualization.
 >- Simply replace the "mySkills.csv" file with your own personally generated one from the live site: https://jac21.github.io/SkillSet/ 
 >- Done and done!
 
-
-Current UI:
+Example Screenshot:
 -----------
 ![Imgur](http://i.imgur.com/l5zoOeg.png)
--- scripts/converter.js
+++ scripts/converter.js
@@ -2,64 +2,72 @@
 var csv = "source,target,value";
 
 // user has uploaded json file and clicked on submit button
-var submitButton = document.getElementById('json-input-button');
-submitButton.addEventListener('click', convertJson, false);
+var submitButton = document.getElementById("json-input-button");
+submitButton.addEventListener("click", convertJson, false);
 
 // user has uploaded converted csv file and clicked on submit button
 var csvInputButton = document.getElementById("csv-input-button");
-csvInputButton.addEventListener('click', visualizeCsv, false);
+csvInputButton.addEventListener("click", visualizeCsv, false);
 
 // error area declaration
-var errorArea = document.getElementById('error-area');
+var errorArea = document.getElementById("error-area");
 
 // convert JSONResume file input to csv file, download to user's machine
 function convertJson(e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	// grab file value, create object URL to use in getJSON call
-	var jsonInput = document.getElementById('json-input');
-	var jsonFileValue = jsonInput.files[0];
-	var jsonFile = window.URL.createObjectURL(jsonFileValue);
+  // grab file value, create object URL to use in getJSON call
+  var jsonInput = document.getElementById("json-input");
+  var jsonFileValue = jsonInput.files[0];
+  var jsonFile = window.URL.createObjectURL(jsonFileValue);
 
-	// if browser supports fetch api
-	if(self.fetch) {
-		fetch(jsonFile)
-		.then(function(response) {
-			return response.json().then(function(json) {
-				// iterate through json resume skill section
-				for (var i = 0; i < json.skills.length; i++) {
-					// iterate through keywords sub-section, append values to csv
-					for (var j = 0; j < json.skills[i].keywords.length; j++) {
-						csv += "\n" + json.skills[i].name + "," + 
-						json.skills[i].keywords[j] + "," + 
-						setSkillValue(json.skills[i].level);
-					}
-				}
-				// clear error area
-				errorArea.innerHTML = '';
+  // if browser supports fetch api
+  if (self.fetch) {
+    fetch(jsonFile)
+      .then(function(response) {
+        return response.json().then(function(json) {
+          // iterate through json resume skill section
+          for (var i = 0; i < json.skills.length; i++) {
+            // iterate through keywords sub-section, append values to csv
+            for (var j = 0; j < json.skills[i].keywords.length; j++) {
+              csv +=
+                "\n" +
+                json.skills[i].name +
+                "," +
+                json.skills[i].keywords[j] +
+                "," +
+                setSkillValue(json.skills[i].level);
+            }
+          }
+          // clear error area
+          errorArea.innerHTML = "";
 
-				// download
-				downloadCsv(csv);
-			});
-		})
-		.catch(function(error) {
-		  errorArea.innerHTML = '<strong>There has been a problem with your fetch operation: ' + error.message + '</strong>';
-		});
-	} else {
-		errorArea.innerHTML = '<strong> Your browser does not support the fetch API! Please try this once more in Chrome or Firefox.</strong>';
-	}
-};
+          // download
+          downloadCsv(csv);
+        });
+      })
+      .catch(function(error) {
+        errorArea.innerHTML =
+          "<strong>There has been a problem with your fetch operation: " +
+          error.message +
+          "</strong>";
+      });
+  } else {
+    errorArea.innerHTML =
+      "<strong> Your browser does not support the fetch API! Please try this once more in Chrome or Firefox.</strong>";
+  }
+}
 
 // create csv object URL from file, call D3
 function visualizeCsv(e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	var csvInput = document.getElementById('csv-input');
-	var csvFileValue = csvInput.files[0];
-	var csvFile = window.URL.createObjectURL(csvFileValue);
+  var csvInput = document.getElementById("csv-input");
+  var csvFileValue = csvInput.files[0];
+  var csvFile = window.URL.createObjectURL(csvFileValue);
 
-	forceLayoutVisualize(csvFile);
-};
+  forceLayoutVisualize(csvFile);
+}
 
 /* 
 	Helper Functions
@@ -67,26 +75,26 @@ function visualizeCsv(e) {
 
 // set skill value numeric value from json value
 function setSkillValue(level) {
-	var skillLevelValue = "0";
+  var skillLevelValue = "0";
 
-	if (level === "Beginner") {
-		skillLevelValue = "1.0";
-	} else if (level === "Intermediate") {
-		skillLevelValue = "2.0";
-	} else if (level === "Advanced") {
-		skillLevelValue = "2.5";
-	}
+  if (level === "Beginner") {
+    skillLevelValue = "1.0";
+  } else if (level === "Intermediate") {
+    skillLevelValue = "2.0";
+  } else if (level === "Advanced") {
+    skillLevelValue = "2.5";
+  }
 
-	return skillLevelValue;
-};
+  return skillLevelValue;
+}
 
 // downloads csv-converted file to local machine
 function downloadCsv(csvString) {
-	var a = document.createElement('a');
-	a.href = 'data:attachment/csv,' +  encodeURIComponent(csvString);
-	a.target = '_blank';
-	a.download = 'mySkills.csv';
+  var a = document.createElement("a");
+  a.href = "data:attachment/csv," + encodeURIComponent(csvString);
+  a.target = "_blank";
+  a.download = "mySkills.csv";
 
-	document.body.appendChild(a);
-	a.click();
-};
+  document.body.appendChild(a);
+  a.click();
+}
